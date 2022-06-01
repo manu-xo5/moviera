f3f0595c7986eca5818c17ec2dc694a68b3459d0
manu-xo5 <mohit.matwaya@gmail.com>
2022-06-01 08:44:59 +0530

    fix navbar logo

--- src/components/link.js
+++ src/components/link.js
@@ -1,5 +1,9 @@
-import { Link as LinkImpl } from "react-router-dom";
+import LinkImpl from 'next/link';
 
 export default function Link({ href, ...props }) {
-  return <LinkImpl to={href} {...props} />;
+  return (
+    <LinkImpl href={href} {...props}>
+      <a {...props} />
+    </LinkImpl>
+  );
 }
